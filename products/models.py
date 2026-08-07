dcfb4d448fc87e89fb09bb8e1e30c52c0546b933
louisae452 <louisae452@msn.com>
2026-08-07 11:04:31 +0100

    Populate product model

--- products/models.py
+++ products/models.py
@@ -23,6 +23,9 @@ class Group(models.Model):
     name = models.CharField(max_length=100)
     slug = models.SlugField(max_length=100, unique=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name
 
 
 class Product(models.Model):
