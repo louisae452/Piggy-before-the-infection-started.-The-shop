8ecbce3bc37797080c631105450b8b5baf37cdaf
louisae452 <louisae452@msn.com>
2026-08-24 14:52:49 +0100

    Add tests for checkout forms

---
+++ checkout/test_forms.py
@@ -0,0 +1,103 @@
+
+from django.test import TestCase
+from .forms import OrderForm, UserForm
+
+
+class TestUserForm(TestCase):
+    """Tests UserForm form"""
+    def test_form_is_valid(self):
+        """Tests the form is valid when all fields are filled in correctly"""
+        data = {
+            'first_name': 'Johan',
+            'last_name': 'Strauss',
+        }
+        user_form = UserForm(data)
+        self.assertTrue(user_form.is_valid())
+
+    def test_form_is_not_valid_first_name(self):
+        """Tests the form is not valid if first_name is missing"""
+        data = {
+            'first_name': '',
+            'last-name': 'Strauss',
+        }
+        user_form = UserForm(data)
+        self.assertFalse(user_form.is_valid())
+
+    def test_form_is_not_valid_last_name(self):
+        """Tests the form is not valid if last_name is missing"""
+        data = {
+            'fisrt_name': 'Johan',
+            'last_name': '',
+        }
+        user_form = UserForm(data)
+        self.assertFalse(user_form.is_valid())
+
+
+class TestOrderForm(TestCase):
+    """Tests OrderForm"""
+    def test_order_form_is_valid(self):
+        """Tests Order form is valid is all appropriate fields are filled in
+            correctly"""
+        data = {
+            'email': 'test@example.com',
+            'street_address1': '23 Mapple Road',
+            'town': 'Lucella',
+            'postcode': 'LU1 3PR'
+        }
+        order_form = OrderForm(data)
+        self.assertTrue(order_form.is_valid())
+
+    def test_order_form_is_not_valid_no_email(self):
+        """Tests OrderForm is not valid if there is no email address"""
+        data = {
+            'email': '',
+            'street_address1': '23 Mapple Road',
+            'town': 'Lucella',
+            'postcode': 'LU1 3PR',
+        }
+        order_form = OrderForm(data)
+        self.assertFalse(order_form.is_valid())
+
+    def test_order_form_is_not_valid_incorrect_email(self):
+        """Tests OrderForm is not valid if email is incorrect"""
+        data = {
+                'email': 'example.com',
+                'street_address1': '23 Mapple Road',
+                'town': 'Lucella',
+                'postcode': 'LU1 3PR',
+            }
+        order_form = OrderForm(data)
+        self.assertFalse(order_form.is_valid())
+
+    def test_order_form_is_not_valid_no_street_address1(self):
+        """Tests OrderForm is not valid if street_address1 is missing"""
+        data = {
+            'email': 'test@example.com',
+            'street_address1': '',
+            'town': 'Lucella',
+            'postcode': 'LU1 3PR',
+        }
+        order_form = OrderForm(data)
+        self.assertFalse(order_form.is_valid())
+
+    def test_order_form_is_not_valid_no_town(self):
+        """Tests OrderForm is not valid if town is missing"""
+        data = {
+            'email': 'test@example.com',
+            'street_address1': '23 Mapple Road',
+            'town': '',
+            'postcode': 'LU1 3PR',
+        }
+        order_form = OrderForm(data)
+        self.assertFalse(order_form.is_valid())
+
+    def test_order_form_is_not_valid_no_postcode(self):
+        """Tests OrderForm is not valid if postcode is missing"""
+        data = {
+            'email': 'test@example.com',
+            'street_address1': '23 Mapple Road',
+            'town': 'Lucella',
+            'postcode': '',
+        }
+        order_form = OrderForm(data)
+        self.assertFalse(order_form.is_valid())
---
+++ readme/tests.md
@@ -0,0 +1,23 @@
+
+### Automated testing.
+
+The following tests were carried out:
+
+
+#### Checkout app.
+
+##### Forms
+
+|Date|Form|Test|Result|Follow-up|
+|:----|:----|:------|:------|:-------|
+|24/08/2026|UserForm|Form is valid when all fields are filled in correctly|Pass||
+|24/08/2026|UserForm|Form is not valid when first_name is missing|Pass||
+|24/08/2026|UserForm|Form is not valid when last_name is missing|Pass||
+|24/08/2026|OrderForm|Form is valid when all required fields are filled in correctly|Pass||
+|24/08/2026|OrderForm|Form is not valid when email is missing|Pass||
+|24/08/2026|OrderForm|Form is not valid when email is incorrect|Pass||
+|24/08/2026|OrederForm|Form is not valid when street_address1 is missing|Pass||
+|24/08/2026|OrderForm|Form is not valid when town is missing|Pass||
+|24/08/2026|OrderForm|Form is not valid when postcode is missing|Pass||
+
+
