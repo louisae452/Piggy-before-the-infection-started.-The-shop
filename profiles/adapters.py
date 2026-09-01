387f4830555b118984986b9fb24e3de4452db1aa
louisae452 <louisae452@msn.com>
2026-09-01 14:03:59 +0100

    Fix profiles adapter

--- profiles/adapters.py
+++ profiles/adapters.py
@@ -8,10 +8,17 @@ class CustomAccountAdapter(DefaultAccountAdapter):
         """Redirects back to the profile dashboard after changing password."""
         return reverse('profiles:profile')
     
-    def get_email_redirect_url(self, request):
-        """Redirects back to the profile dashboard after adding, removing, or changing or resending verification."""
-        return reverse('profiles:profile')
+    # def get_email_redirect_url(self, request):
+    #    """Redirects back to the profile dashboard after adding, removing, or changing or resending verification."""
+    #    return reverse('profiles:profile')
     
     def get_login_redirect_url(self, request):
         """Redirects back to the profile dashboard immediately after adding a new email address."""
+        return reverse('profiles:profile')
+    
+    def get_email_confirmation_redirect_url(self, request):
+        """
+        Handles the redirect immediately after a user clicks the 
+        verification link inside their email inbox.
+        """
         return reverse('profiles:profile')
